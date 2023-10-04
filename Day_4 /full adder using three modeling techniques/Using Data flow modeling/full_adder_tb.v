196ce775f45d77dc977325af758330dc495edee4
deva2711 <108519518+deva2711@users.noreply.github.com>
2023-10-04 22:39:53 +0530

    Create full_adder_tb.v

---
+++ Day_4 /full adder using three modeling techniques/Using Data flow modeling/full_adder_tb.v
@@ -0,0 +1,28 @@
+
+// testbench for full adder
+module full_adder_tb;
+  
+  // set up veriable
+  reg x,y,c_in;
+  wire sum_f,carry_f ;
+  
+  // instantiation of design module
+  full_adder dut(x,y,c_in,sum_f,carry_f);
+      initial
+	begin
+      x = 0 ; y = 0 ; c_in= 0 ;
+      $monitor("x=%b y=%b C_in=%b sum =%b carry=%b ",x,y,c_in,sum_f,carry_f);
+      $dumpfile("dump.vcd"); 
+      $dumpvars;
+	#200 $finish;
+	end
+    // given input using random
+  always 
+    begin 
+      #5;
+      x = $random;
+      y = $random;
+      c_in = $random;
+    end
+    
+endmodule 
