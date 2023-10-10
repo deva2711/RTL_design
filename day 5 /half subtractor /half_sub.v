bb5276ee24b852f6b60ad49bf316aa2b0c926809
deva2711 <108519518+deva2711@users.noreply.github.com>
2023-10-10 21:07:30 +0530

    Create half_sub.v

---
+++ day 5 /half subtractor /half_sub.v
@@ -0,0 +1,16 @@
+/****************************************************
+Developer    : Devang vekariya
+Module       : half subtractor 
+Institute    : NIT Kurukshetra
+Date created : 04/10/2023
+********************************************************/
+
+// full adder module
+module half_sub(x,y,diff,borrow);
+  input x, y;
+  output diff,borrow;
+  
+  assign diff = x ^ y;
+  assign borrow = ~x && y ;
+  
+endmodule
