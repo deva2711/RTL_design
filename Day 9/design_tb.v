4c14928c6d88c018be51df2acae9ff0e31ba84a0
deva2711 <108519518+deva2711@users.noreply.github.com>
2024-01-01 19:59:07 +0530

    Create design_tb.v

---
+++ Day 9/design_tb.v
@@ -0,0 +1,16 @@
+module tb;
+  reg [31:0]a,b ;
+  wire [31:0] sum ;
+
+
+  top_module (a,b,sum);
+
+
+  initial
+    begin
+  a = 6 ; b = 7 ;
+  #2 a =7 ,b= 17 ;
+  #5 b=6 , c= 16;
+    end
+endmodule 
+
